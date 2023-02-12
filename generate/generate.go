e28389514fe2bb1d64a51877c2ea0477d1469ca3
atborg <33327789+atborg@users.noreply.github.com>
2023-02-12 01:41:52 -0700

    tui and select random letters

--- generate/generate.go
+++ generate/generate.go
@@ -1,10 +1,31 @@
 package generate
 
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 type HiraganaCharacter struct {
-	Hiragana rune
+	Hiragana string
 	Latin    string
 }
 
-func GenerateHiragana() []HiraganaCharacter {
-	return []HiraganaCharacter{{'あ', "a"}, {'い', "i"}, {'う', "u"}, {'え', "e"}, {'お', "o"}}
+func GenerateHiragana(options int) (HiraganaCharacter, []HiraganaCharacter) {
+	hiraganaCharacters := []HiraganaCharacter{{"あ", "a"}, {"い", "i"}, {"う", "u"}, {"え", "e"}, {"お", "o"}}
+	choices := []HiraganaCharacter{}
+
+	rand.Shuffle(len(hiraganaCharacters), func(i, j int) {
+		hiraganaCharacters[i], hiraganaCharacters[j] = hiraganaCharacters[j], hiraganaCharacters[i]
+	})
+
+	for i := 0; i < options; i++ {
+		choices = append(choices, hiraganaCharacters[i])
+	}
+
+	return choices[rand.Intn(len(choices))], choices
+
 }
--- main.go
+++ main.go
@@ -2,13 +2,89 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+
 	"github.com/borghippo/hakusan/generate"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func main() {
-	hiraganaCharacters := generate.GenerateHiragana()
+const numOptions = 5
 
-	for _, char := range hiraganaCharacters {
-		fmt.Printf("hiragana: %c latin: %s\n", char.Hiragana, char.Latin)
+var ans, choices = generate.GenerateHiragana(numOptions)
+
+type model struct {
+	cursor int
+	choice generate.HiraganaCharacter
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+
+		case "enter":
+			// Send the choice on the channel and exit.
+			m.choice = choices[m.cursor]
+			return m, tea.Quit
+
+		case "down", "j":
+			m.cursor++
+			if m.cursor >= len(choices) {
+				m.cursor = 0
+			}
+
+		case "up", "k":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(choices) - 1
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := strings.Builder{}
+	s.WriteString(ans.Hiragana + "\n\n")
+
+	for i := 0; i < len(choices); i++ {
+		if m.cursor == i {
+			s.WriteString("(•) ")
+		} else {
+			s.WriteString("( ) ")
+		}
+		s.WriteString(choices[i].Latin)
+		s.WriteString("\n")
+	}
+	s.WriteString("\n(press q to quit)\n")
+
+	return s.String()
+}
+
+func main() {
+	p := tea.NewProgram(model{})
+
+	// Run returns the model as a tea.Model.
+	m, err := p.Run()
+	if err != nil {
+		fmt.Println("Oh no:", err)
+		os.Exit(1)
+	}
+
+	// Assert the final tea.Model to our local model and print the choice.
+	if m, ok := m.(model); ok && m.choice.Latin != "" {
+		if m.choice.Latin == ans.Latin {
+			fmt.Printf("\n---\nCorrect! %s is %s\n", m.choice.Hiragana, m.choice.Latin)
+		} else {
+			fmt.Printf("\n---\nWrong... %s is %s\n", ans.Hiragana, ans.Latin)
+		}
 	}
 }
